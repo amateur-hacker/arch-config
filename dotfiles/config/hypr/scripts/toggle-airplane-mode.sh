59d50741599148763fc58b4cd752e5c1f46d5be1
amateur-hacker <amateurhacker2004@gmail.com>
2026-03-17 17:03:00 +0530

    made changes

--- dotfiles/config/hypr/scripts/toggle-airplane-mode.sh
+++ dotfiles/config/hypr/scripts/toggle-airplane-mode.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 wifi="$(nmcli r wifi | awk 'FNR = 2 {print $1}')"
 if [ "$wifi" == "enabled" ]; then
   rfkill block all &
-  notify-send -t 1000 'airplane mode: active'
 else
   rfkill unblock all &
-  notify-send -t 1000 'airplane mode: inactive'
 fi
